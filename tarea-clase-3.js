765b73d5c51a0b18b6bab25f49a1aedfb1f30195
Santiago-Musso <santiagomusso@gmail.com>
2022-05-25 17:49:10 -0300

    Resolución tarea clase 3

--- tarea-clase-3.js
+++ tarea-clase-3.js
@@ -7,9 +7,32 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
+const nombreUsuario = prompt("Cual es su nombre?")
+const nombreAmigo = "Benjamin"
+const nombreAdmin = "Santiago"
+if (nombreUsuario === nombreAdmin){
+    console.log("Hola, Tocayo! Yo también me llamo " + nombreUsuario)
+}else if (nombreUsuario === nombreAmigo){
+    console.log("Hola " + nombreUsuario + " te llamás igual que mi amigo")
+}else {
+    console.log("Hola "+ nombreUsuario + "!")
+}
+
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+const edadUsuario = Number(prompt("Cuantos años tienes?"))
+const edadAdmin = 22
+if (edadUsuario === edadAdmin){
+    console.log("Tienes la misma edad que yo!")
+}else if (edadUsuario >= edadAdmin){
+    console.log("Eres mas grande que yo!")
+}else if (edadUsuario <= edadAdmin){
+    console.log("Eres menor que yo!")
+}else {
+    console.log("No he entendido lo que me dices")
+}
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
@@ -19,3 +42,20 @@
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+const tieneDocumento = prompt("Tienes documento? (Si/No)")
+
+if (tieneDocumento.toLowerCase() === 'si'){
+    let edadUsuario = Number(prompt("Cuantos años tienes?"))
+    if (edadUsuario >= 18){
+        console.log("Puedes entrar al bar")
+    }else if (edadUsuario <= 18){
+        console.log("No puedes entrar al bar")
+    }else {
+        console.log("No he entendido lo que me dices")
+    }
+}else if (tieneDocumento.toLowerCase() === 'no'){
+    console.log("No puedes entrar al bar")
+}else {
+    console.log("No he entendido lo que me dices")
+}
