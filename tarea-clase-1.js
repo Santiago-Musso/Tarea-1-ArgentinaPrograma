04ae60fa6679c61c2df365e622ca1b1fd1a33194
Santiago-Musso <santiagomusso@gmail.com>
2022-05-24 22:19:59 -0300

    Resolución de ejercicios
    Se resolvieron las consignas que pedía cada ejercicio

--- tarea-clase-1.js
+++ tarea-clase-1.js
@@ -3,8 +3,33 @@
 // Preguntarle estos datos al usuario y guardarlos en 2 variables
 // Ejecutar la función con estos datos
 // Impriman el resultado en la consola
+function edadUsuario(anioActual,anioNacimiento){
+    return anioActual - anioNacimiento
+}
 
+const anioActual = parseInt(prompt("En qué año estamos?"))
+const anioNacimiento = parseInt(prompt("En qué año naciste?"))
+
+console.log(edadUsuario(anioActual,anioNacimiento))
 
 // Preguntar el salario anual y calcular el salario mensual
 // Preguntar el salario mensual y calcular el anual
 // diario... semanal, por hora. etc.
+
+const salarioAnual = parseInt(prompt("Cual es tu salario anual?"))
+
+function calcularSalarioAnual(salarioAnual){
+    return salarioAnual / 12
+}
+alert(`Tu salario mensual es de: $${parseInt(calcularSalarioAnual(salarioAnual))}`)
+
+const salarioMensual = parseInt(prompt("Cual es tu salario mensual?"))
+calcularSalario(salarioMensual)
+
+function calcularSalario(salarioMensual){
+    const $salarioAnual = salarioMensual*12
+    const salarioSemanal = $salarioAnual / 52
+    const salarioDiario = salarioSemanal / 5
+    const salarioHora = salarioDiario / 8
+    return alert(`Tu salario anual es de $${parseInt($salarioAnual)}, el semanal de $${parseInt(salarioSemanal)}, el diario de $${parseInt(salarioDiario)} y por hora de $${parseInt(salarioHora)}:`);
+}
