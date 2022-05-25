5feb5922ca26de2738826d7a6a5cd012a262b434
Santiago-Musso <santiagomusso@gmail.com>
2022-05-24 21:39:42 -0300

    Tarea Clase1 (default)

---
+++ tarea-clase-1.js
@@ -0,0 +1,10 @@
+// crear una función que tome como parámetro el año actual y el año de nacimiento
+// y calcule la edad del usuario (más o menos).
+// Preguntarle estos datos al usuario y guardarlos en 2 variables
+// Ejecutar la función con estos datos
+// Impriman el resultado en la consola
+
+
+// Preguntar el salario anual y calcular el salario mensual
+// Preguntar el salario mensual y calcular el anual
+// diario... semanal, por hora. etc.
