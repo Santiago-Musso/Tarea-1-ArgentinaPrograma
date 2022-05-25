d435ffd9104dbe1f30d103a167604e99dd5959b6
Santiago-Musso <santiagomusso@gmail.com>
2022-05-25 17:16:27 -0300

    Correcciones tarea 2
    Se cambio el return de las funciones para que solo devuelvan el dato que se necesita, se cambio a comparaciones estrictas ===  , y se corrigio la validacion de si es un numero con la implementacion del metodo isNaN(valor)

--- Tarea 2/tarea-clase-2.js
+++ Tarea 2/tarea-clase-2.js
@@ -1,14 +1,14 @@
 function sumar(numero1, numero2){
-    return alert("El resultado de la suma es: " + (numero1 + numero2).toFixed(2))
+    return (numero1 + numero2).toFixed(2)
 }
 function restar(numero1,numero2){
-    return alert("El resultado de la resta es: " + (numero1 - numero2).toFixed(2))
+    return (numero1 - numero2).toFixed(2)
 }
 function multiplicar(numero1,numero2){
-    return alert("El resultado de la multiplicacion es: " + (numero1 * numero2).toFixed(2))
+    return (numero1 * numero2).toFixed(2)
 }
 function dividir(numero1,numero2){
-    return alert("El resultado de la division es: " + (numero1 / numero2).toFixed(2))
+    return (numero1 / numero2).toFixed(2)
 }
 
 // TAREA: Tenemos 2 funciones de la tarea anterior -- 'sumar' y 'restar'
@@ -22,39 +22,39 @@ function dividir(numero1,numero2){
 // Paso 2 - Crear 2 variables que contengan 2 números cualquiera.
 // Paso 3 - Crear una condición if/else basada en el operador que tenemos.
 //
-var salir = false
+let salir = false
 do {
     operador = prompt("Que operacion desea realizar? (+)sumar, (-)restar, (*)multiplicar, (/)dividir o (S)salir")
-    if(operador == "+" || operador == "-" || operador == "*" || operador == "/"){
+    if(operador === "+" || operador === "-" || operador === "*" || operador === "/"){
         salir = true
     }else if (operador != "S" && operador != "s"){
         alert("Ingrese una opcion válida")
     }else{
         salir = true
     }
-}while(salir == false)
+}while(salir === false)
 
-if (operador == "+" || operador == "-" || operador == "*" || operador == "/"){    
+if (operador === "+" || operador === "-" || operador === "*" || operador === "/"){    
     do{
         numero1 = parseFloat(prompt("Inserte el primer numero"))
         numero2 = parseFloat(prompt("Inserte el segundo numero"))
-        if (numero1 == NaN || numero2 == NaN){
+        if (isNaN(numero1) || isNaN(numero2)){
             alert("No ha introducido un valor correcto")
             salir = false
         }
-    }while(salir == false)
+    }while(salir === false)
 
     if (operador == "+"){
-        sumar(numero1, numero2)
-    }else if (operador == "-"){
-        restar(numero1,numero2)
-    }else if (operador == "*"){
-        multiplicar(numero1,numero2)
+        alert("El resultado de la suma es: "+ sumar(numero1, numero2))
+    }else if (operador === "-"){
+        alert("El resultado de la resta es: " + restar(numero1,numero2))
+    }else if (operador === "*"){
+        alert("El resultado de la multiplicacion es: " + multiplicar(numero1,numero2))
     }else{
-        if(numero2 == 0){
+        if(numero2 === 0){
             alert("No se puede dividir por 0")
         }else{
-            dividir(numero1,numero2)
+            alert("El resultado de la division es: " + dividir(numero1,numero2))
         }
     }
 }
