cf22591da640b23b95f9c7736086322160f11ca4
Santiago-Musso <santiagomusso@gmail.com>
2022-05-30 18:14:09 -0300

    Cambio de carpeta

---
+++ Tarea 4 algoritmos/tarea-clase-4-algortimos.js
@@ -0,0 +1,32 @@
+function desafioProgramacion1 (){
+    
+    const NUMEROS_MINIMOS = 1
+    const NUMEROS_MAXIMOS = 10
+
+    function impresorNumeros(){
+        for (let i = NUMEROS_MAXIMOS; i >= NUMEROS_MINIMOS; i--){
+            console.log(i)
+        }
+    }
+
+    impresorNumeros()
+}
+
+function desafioProgramacion2(){
+    console.log("Desafio de programacion #2: ")
+
+    const NUMEROS_MINIMOS = 1
+    const NUMEROS_MAXIMOS = 100
+
+    function impresorNumerosImpares(){
+        for (let i = NUMEROS_MINIMOS; i <= NUMEROS_MAXIMOS; i += 2){
+            console.log(i)
+        }
+    }
+    impresorNumerosImpares()
+}
+
+function desafioProgramacion3(){
+    console.log("Desafio de programacion #3")
+    const NUMERO_A_MULTIPLICAR = 7
+}
