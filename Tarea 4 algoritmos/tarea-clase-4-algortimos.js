9d2c5ede9e150d6b469a472af380828fbc47a26b
Santiago-Musso <santiagomusso@gmail.com>
2022-05-31 00:22:27 -0300

    Agregado de consignas

--- Tarea 4 algoritmos/tarea-clase-4-algortimos.js
+++ Tarea 4 algoritmos/tarea-clase-4-algortimos.js
@@ -1,5 +1,7 @@
 function desafioProgramacion1 (){
     console.log("Desafio de programacion #1: ")
+    //Desafío de programación #1: Imprimí los números del 1 al 10 
+    //Resultado: 10 9 8 7 6 5 4 3 2 1
 
     const NUMEROS_MINIMOS = 1
     const NUMEROS_MAXIMOS = 10
@@ -15,6 +17,7 @@ function desafioProgramacion1 (){
 
 function desafioProgramacion2(){
     console.log("Desafio de programacion #2: ")
+    //Desafìo de programación  #2: Imprimí los números impares del 1 al 100
 
     const NUMEROS_MINIMOS = 1
     const NUMEROS_MAXIMOS = 100
@@ -29,6 +32,8 @@ function desafioProgramacion2(){
 
 function desafioProgramacion3(numeroMultiplicar){
     console.log("Desafio de programacion #3: ")
+    //Desafío de programación #3: Imprimí la tabla de multiplicación del 7
+
     const INICIO_TABLA = 0
     const FIN_TABLA = 9
 
@@ -42,6 +47,8 @@ function desafioProgramacion3(numeroMultiplicar){
 
 function desafioProgramacion4(){
     console.log("Desafio de programacion #4:")
+    //Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 9
+
     const NUMERO_INICIO = 1 
     const NUMERO_FIN = 9
 
@@ -53,6 +60,8 @@ function desafioProgramacion4(){
 
 function desafioProgramacion5(){
     console.log("Desafio de programacion #5: ")
+    //Desafío de programación #5: Calcula el resultado de la suma de los numeros del 1 al 10 en un array.
+
     const ARREGLO_NUMEROS = [1,2,3,4,5,6,7,8,9,10]
 
     function sumarArreglo(){
@@ -67,6 +76,8 @@ function desafioProgramacion5(){
 
 function desafioProgramacion6(){
     console.log("Desafio de programacion #6: ")
+    //Desafío de programación #6: Calcula 10! (10 factorial)
+
     const NUMERO_FACTORIAL = 10
 
     function hacerFactorialNumero(){
@@ -81,6 +92,8 @@ function desafioProgramacion6(){
 
 function desafioProgramacion7(){
     console.log("Desafio de programacion #7: ")
+    //Desafío de programación #7: Calcula la suma de todos los números impares mayores que 10 y menores que 30
+
     const NUMEROS_MAYOR_QUE = 10
     const NUMEROS_MENOR_QUE = 15
 
@@ -98,6 +111,8 @@ function desafioProgramacion7(){
 
 function desafioProgramacion8(){
     console.log("Desafio de programacion #8: ")
+    //Desafío de programación #8: Crea una función que convierta de Celsius a Fahrenheit
+
     const CELSIUS_A_CONVERTIR = 26
 
     function convertidorAFahrenheit(){
@@ -108,16 +123,23 @@ function desafioProgramacion8(){
 
 function desafioProgramacion9(){
     console.log("Desafio de programacion #9: ")
-        const FAHRENHEIT_A_CONVERTIR = 77
+    //Desafío de programación #9: Crea una función que convierta de Fahrenheit a Celsius
 
-        function convertidorACelsius(){
-            return (FAHRENHEIT_A_CONVERTIR - 32) * 5/9
-        }
+    const FAHRENHEIT_A_CONVERTIR = 77
+
+    function convertidorACelsius(){
+        return (FAHRENHEIT_A_CONVERTIR - 32) * 5/9
+    }
     console.log(convertidorACelsius())
 }
 
+//Desafío de programación #10: Calcula la suma de todos los números en un array de números
+//Resuelto en el desafio #5
+
 function desafioProgramacion11(){
     console.log("Desafio de programacion #11: ")
+    //Desafío de programación #11: Calcula el promedio de todos los números en un array de números. (en una función)
+
     const ARREGLO_NUMEROS = [1,2,3,4,5,6,7,8,9,10,12]
 
     function promediarArreglo(){
@@ -133,6 +155,8 @@ function desafioProgramacion11(){
 
 function desafioProgramacion12(){
     console.log("Desafio de programacion #12: ")
+    //Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos
+
     const ARREGLO_A_CONVERTIR = [1,3,6,-8,8,-15,-5]
 
     function convertirSoloPositivos(){
@@ -149,6 +173,8 @@ function desafioProgramacion12(){
 
 function desafioProgramacion13(){
     console.log("Desafio de programacion #13: ")
+    //Desafío de programación #13: Find the maximum number in an array of numbers
+
     const ARREGLO_NUMEROS = [1,25,6356,3,85,756]
 
     function buscarMaximoArreglo(){
@@ -165,6 +191,8 @@ function desafioProgramacion13(){
 
 function desafioProgramacion14(){
     console.log("Desafio de programacion #14: ")
+    //Desafío de programación #14: Imprimir los primeros 10 dígitos de Fibonacci sin recursión
+
     const NUMEROS_A_IMPRIMIR = 16
 
     function imprimirFibonacciHasta(){
@@ -188,6 +216,7 @@ function desafioProgramacion14(){
 
 function desafioProgramacion15(){
     console.log("Desafio de programacion #15: ")
+    //Desafío de programación #15: Crear una función que encuentre el número n de Fibonacci usando recursión
 
     function imprimirFibonacci(NUMERO_N = 5){
         if (NUMERO_N === 0){
@@ -203,6 +232,8 @@ function desafioProgramacion15(){
 
 function desafioProgramacion17(){
     console.log("Desafio de programacion #17: ")
+    //Desafío de programación #17: Calcular la suma de los dígitos positivos de un número entero positivo
+
     const NUMERO_A_SUMAR = "100000000005"
     let resultado = 0
     
@@ -219,6 +250,8 @@ function desafioProgramacion17(){
 
 function desafioProgramacion20(){
     console.log("Desafio de programacion #20: ")
+    //Desafío de programación #20: Rotar un array hacia la izquierda una posición
+
     const ARREGLO_A_ROTAR = [4,3,2,1]
     let resultado = []
 
@@ -237,6 +270,8 @@ function desafioProgramacion20(){
 
 function desafioProgramacion21(){
     console.log("Desafio de programacion #21: ")
+    //Desafío de programación #21: Rotar un array a la derecha una posición
+
     const ARREGLO_A_ROTAR = [2,3,4,1]
     let resultado = []
 
@@ -255,6 +290,8 @@ function desafioProgramacion21(){
 
 function desafioProgramacion22(){
     console.log("Desafio de programacion #22: ")
+    //Desafío de programación #22: Invertir un array
+
     const ARREGLO_A_ROTAR = [1,2,3,4]
     let resultado = []
     
@@ -273,6 +310,8 @@ function desafioProgramacion22(){
 
 function desafioProgramacion23(){
     console.log("Desafio de programacion #23: ")
+    //Desafío de programación #23: Invertir una cadena de caracteres
+
     const STRING_A_ROTAR = "dabale arroz a la zorra el abad"
     let resultado = []
 
@@ -287,6 +326,8 @@ function desafioProgramacion23(){
 
 function desafioProgramacion24(){
     console.log("Desafio de programacion #24: ")
+    //Desafío de programación #24: Crear una función que reciba dos dos arrays (arreglos) como argumentos y returne el resultado en un nuevo arreglo
+
     const STRING_A_ENVIAR_1 = [1,2,3]
     const STRING_A_ENVIAR_2 = ["a","b","c"]
     let resultado = []
@@ -305,6 +346,8 @@ function desafioProgramacion24(){
 
 function desafioProgramacion25(){
     console.log("Desafio de programacion #25: ")
+    //Desafío de programación #25: Crear una función que reciba dos arrays (arreglos) de números como argumentos y retorne un array con números que estén en uno u otro array, pero NO en ambos.
+
     const ARREGLO_NUMEROS_1 = [5,10,3]
     const ARREGLO_NUMEROS_2 = [10,1]
     let resultado = []
@@ -347,6 +390,9 @@ function desafioProgramacion25(){
 }
 
 function desafioProgramacion26(){
+    console.log("Desafio de programacion #26: ")
+    //Desafío de programación #26: Crear una función que reciba dos arrays de números y retorne un nuevo array con los elementos que se encuentren en el primer array, pero no en el segundo
+
     const ARREGLO_A_RESTAR_1 = [5,1,2,3,4]
     const ARREGLO_A_RESTAR_2 = [3,4]
     let resultado = []
@@ -388,6 +434,9 @@ function desafioProgramacion26(){
 }
 
 function desafioProgramacion27(){
+    console.log("Desafio de programacion #27: ")
+    //Desafío de programación #27: Crear una función que reciba un array de números como argumento y retorne un array con los elementos distintos
+
     const ARREGLO = [1,2,3,4,5,4,3,2,1,0]
     let resultado = []
 
